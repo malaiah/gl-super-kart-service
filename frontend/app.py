2cf67839a4b76676b476f47a7850ba951c9508c6
M CMG <malaiah@users.noreply.github.com>
2026-08-15 13:19:00 -0400

    Update frontend/app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,12 +2,12 @@
 import streamlit as st
 import requests
 
-st.title("___Super Kart UI__") #Complete the code to define the title of the app.
+st.title("___Super Kart UI__") 
 
 # Input fields for product and store data
 Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
 Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
-Product_Allocated_Area =  st.number_input("Product Allocated Area") 
+Product_Allocated_Area =  st.number_input("Product Allocated Area")
 Product_MRP = st.number_input("Product MRP")
 Store_Size = st.number_input("Store Size")
 Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
@@ -30,7 +30,7 @@ product_data = {
 }
 
 if st.button("Predict", type='primary'):
-    response = requests.post("https://gl-super-kart-service.app.github.dev/v1/predict", json=product_data)    
+    response = requests.post("https://humble-couscous-w4499jqw7wfg645.github.dev/v1/predict", json=product_data)
     if response.status_code == 200:
         result = response.json()
         predicted_sales = result["Sales"]
